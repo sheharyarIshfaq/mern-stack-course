f887382990660cc994b8193f8a2546275ea32af2
sheharyarIshfaq <sheharyarishfaq@gmail.com>
2021-10-15 19:13:56 +0500

    Added Nav Links in Navigation Menu

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
 import "./MainNavigation.css";
 
 const MainNavigation = () => {
@@ -14,7 +16,9 @@ const MainNavigation = () => {
       <h1 className="main-navigation__title">
         <Link to="/">Your Places</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 };
---
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import "./NavLink.css";
+const NavLinks = () => {
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/" exact>
+          All Users
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/uId/places">My Places</NavLink>
+      </li>
+      <li>
+        <NavLink to="/places/new">Add Place</NavLink>
+      </li>
+      <li>
+        <NavLink to="/auth">Authenticate</NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default NavLinks;
