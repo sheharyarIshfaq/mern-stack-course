27ba903f774facda50be6d515511625a5bd0f0dc
sheharyarIshfaq <sheharyarishfaq@gmail.com>
2021-10-25 09:34:31 +0500

    Working on conneccting frontend with backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,16 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/places", placesRouter);
 app.use("/api/users", usersRouter);
 
@@ -30,7 +40,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://sheharyar:sheharyar@cluster0.pwoor.mongodb.net/places?retryWrites=true&w=majority"
+    "mongodb+srv://sheharyar:sheharyar@cluster0.pwoor.mongodb.net/mern?retryWrites=true&w=majority"
   )
   .then(
     app.listen(port, () => {
