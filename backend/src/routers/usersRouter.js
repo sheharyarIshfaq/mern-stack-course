73547e492323abae384e4332f812fbbfc63f4a44
sheharyarIshfaq <sheharyarishfaq@gmail.com>
2021-10-28 18:08:40 +0500

    Using multer to store images to the backend

--- backend/src/routers/usersRouter.js
+++ backend/src/routers/usersRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { check } = require("express-validator");
 const usersControllers = require("../controllers/users-controller");
+const fileUpload = require("../middleware/file-upload");
 
 const router = express.Router();
 
@@ -8,6 +9,7 @@ router.get("/", usersControllers.getUsers);
 
 router.post(
   "/signup",
+  fileUpload.single("image"),
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
